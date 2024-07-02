20e74f408c4b6b99ab4d35f6aaf5b90c9eba90a0
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-07-02 02:54:05 -0400

    commmit

--- etl_toll_data.py
+++ etl_toll_data.py
@@ -30,57 +30,69 @@ def unzip_data():
     try:
         # Define paths
         source_file = '/Users/jbshome/Desktop/python-etl-pipeline/staging/tolldata.tgz'
+        logging.info('Source_file was successfuly reached.')
         destination_folder = '/Users/jbshome/Desktop/python-etl-pipeline/destination_folder'
+        logging.info('Data sent to destination folder.')
     
         with tarfile.open(source_file, 'r:gz') as tar:
                 tar.extractall(destination_folder)
-                print(f"Extraction complete. Files extracted to {destination_folder}")
+                logging.info(f"Extraction complete. Files extracted to {destination_folder}")
 
         return
-    except:
-
+    except FileNotFoundError:
+        logging.error(f"Source file not found: {source_file}")
+    except tarfile.ReadError:
+        logging.error(f"Failed to read tar file: {source_file}")
+    except Exception as e:
+        logging.error(f"Could not extract the files from the compressed tgz file. Error: {e}")
 
 # call the function
 unzip_data()
 
 # Function to extract data from the csv file
 def extract_data_from_csv():
-    csv_file_path = '/Users/jbshome/Desktop/python-etl-pipeline/files/destination_folder/vehicle-data.csv'
+    try:
+        # Define path and column names
+        csv_file_path = "/Users/jbshome/Desktop/python-etl-pipeline/files/destination_folder/vehicle-data.csv"
+        column_names = ["Rowid", "Timestamp", "Anonymized_vehicle_number", "Vehicle_type", "Number_of_axles", "Vehicle_code"]
+        logging.info("Reading CSV file from: %s", csv_file_path)
+        # Reading the csv file.
+        data = pd.read_csv(csv_file_path, names=column_names)
+        # For this data, we only need the first 4 columns. That is why i first named the columns to safely remove the columns I do not need.
+        data = data.drop(["Number_of_axles", "Vehicle_code"], axis=1)
+        # Save data to a csv file
+        data.to_csv(csv_data, index=False)
+        logging.info("Data saved successfuly.")
+        
+        return data
+    except FileNotFoundError:
+        logging.info("CSV file not found: %s", csv_file_path)
+    except pd.errors.ParserError:
+        logging.info("Error parsing CSV file: %s", csv_file_path)
+    except Exception as e:
+        logging.info("An error occurred: %s", str(e))
 
-    # The data does not have column name, so I will specify my own column names.
-    column_names = ['Rowid', 'Timestamp', 'Anonymized_vehicle_number', 'Vehicle_type', 'Number_of_axles', 'Vehicle_code']
-
-    # Reading the csv file.
-    data = pd.read_csv(csv_file_path, names=column_names)
-    
-    # For this data, we only need the first 4 columns. That is why i first named the columns to safely remove the columns I do not need.
-    data = data.drop(['Number_of_axles', 'Vehicle_code'], axis=1)
-
-    # Save data to a csv file
-    data.to_csv(csv_data, index=False)
-    
-    return data
 
 # call the function
 extract_data_from_csv()
 
 # Function to extract data from the tsv file
 def extract_data_from_tsv():
-    tsv_file_path = '/Users/jbshome/Desktop/python-etl-pipeline/files/destination_folder/tollplaza-data.tsv'
-
-    # The data does not have column names, so I will specify my own column names to safely remove the data I do not need.
-    column_names = ['Rowid', 'Timestamp', 'Anonymized_vehicle_number', 'Vehicle_type', 'Number_of_axles', 'Tollplaza_id', 'Tollplaza_code']
-
-    # Reading the tsv file.
-    data = pd.read_csv(tsv_file_path, delimiter='\t', names=column_names)
-    
-    # For this data, we only need the last three columns, so I am removing the first 4 columns because they already exist in the csv data.
-    data = data.drop(['Rowid', 'Timestamp', 'Anonymized_vehicle_number', 'Vehicle_type'], axis=1)
-    
-    # Save data to a csv file
-    data.to_csv(tsv_data, index=False)
-    
-    return data
+    try:
+        # Define path and column names
+        tsv_file_path = '/Users/jbshome/Desktop/python-etl-pipeline/files/destination_folder/tollplaza-data.tsv'
+        column_names = ['Rowid', 'Timestamp', 'Anonymized_vehicle_number', 'Vehicle_type', 'Number_of_axles', 'Tollplaza_id', 'Tollplaza_code']
+        logging.info("Reading TSV file from: %s", tsv_file_path)
+        # Reading the tsv file.
+        data = pd.read_csv(tsv_file_path, delimiter='\t', names=column_names)
+        # For this data, we only need the last three columns, so I am removing the first 4 columns because they already exist in the csv data.
+        data = data.drop(['Rowid', 'Timestamp', 'Anonymized_vehicle_number', 'Vehicle_type'], axis=1)
+        
+        # Save data to a csv file
+        data.to_csv(tsv_data, index=False)
+        logging.info("Data saved successfuly.")
+        
+        return data
 # call the function
 extract_data_from_tsv()
 
